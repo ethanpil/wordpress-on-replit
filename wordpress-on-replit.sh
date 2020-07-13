77a17524d3ca045ceef4cc63ff23199261faec59
ethanpil <ethan@piliavin.com>
2020-07-12 17:22:30 -0700

    Testing new curl command with logging and wp-cli hack

--- wordpress-on-replit.sh
+++ wordpress-on-replit.sh
@@ -2,18 +2,17 @@
 # Install WordPress on Repl.it
 # 
 # Create a new Repl.it as a PHPWebServer
-# wget -O - https://raw.githubusercontent.com/ethanpil/wordpress-on-replit/master/wordpress-on-replit.sh | bash
+# curl -O https://raw.githubusercontent.com/ethanpil/wordpress-on-replit.sh/master/wordpress-on-replit.sh && chmod 0700 wordpress-on-replit.sh && echo "Starting install." && bash wordpress-on-replit.sh > wordpress-on-replit.log && echo "Complete. See wordpress-on-replit.log"
 
 #Download, extract and cleanup required missing PHP modules
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/php7.2/php7.2-mysql_7.2.24-0ubuntu0.18.04.6_amd64.deb
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/php7.2/php7.2-sqlite3_7.2.24-0ubuntu0.18.04.6_amd64.deb
 for Module in $( ls php*.deb ); do dpkg -x $Module .; done
-mkdir php
-cp usr/lib/php/*/* ./php
-mv mysqlnd.so A-mysqlnd.so
-echo 'extension=pdo.so' > php/php.ini
-mv ./php/mysqlnd.so ./php/A-mysqlnd.so #Ensure mysqlnd.so is loaded before mysqli.so
-for Module in $( ls php/*.so ); do echo "extension=$Module" >> php/php.ini; done
+mkdir ~/php
+cp usr/lib/php/*/* ~/php
+echo 'extension=pdo.so' > ~/php/php.ini
+mv ~/php/mysqlnd.so ~/php/A-mysqlnd.so #Ensure mysqlnd.so is loaded before mysqli.so
+for Module in $( ls ~/php/*.so ); do echo "extension=$Module" >> ~/php/php.ini; done
 rm -rf ./etc/ 
 rm -rf ./usr/
 rm php7.2-mysql_7.2.24-0ubuntu0.18.04.6_amd64.deb
@@ -37,8 +36,22 @@ unzip sqlite-integration.1.8.1.zip
 rm sqlite-integration.1.8.1.zip
 cp ./sqlite-integration/db.php ..
 
+#Download, extract and setup wp-cli and dependencies
+cd ./../../..
+wget http://archive.ubuntu.com/ubuntu/pool/main/l/less/less_551-2_amd64.deb
+for Module in $( ls less*.deb ); do dpkg -x $Module .; done
+mv ./usr/ ~
+rm -rf less*
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar ~/php/wp-cli.phar
+
+#hacky way to get wp-cli working in limited repli.it shell
+cat >wordpress/wp <<EOL
+#!/bin/bash
+export PATH=$PATH:~/usr/bin:~:~/php
+php -c ~/php/php.ini ~/php/wp-cli.phar
+EOL
+
 #Setup the repl to start PHP with the correct php.ini that includes our modules
-echo 'run = "bash run.sh"' > .replit
-echo '#!/bin/bash' > run.sh
-echo 'export PATH=$PATH:~/usr/bin:~' >> run.sh
-echo 'php -c php/php.ini -S 0.0.0.0:8000 -t wordpress/' >> run.sh
+echo 'run = "php -c ~/php/php.ini -S 0.0.0.0:8000 -t wordpress/"' >> .replit
