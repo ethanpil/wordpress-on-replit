3cd31a6a40103e26fa7cfa5fb42de31c474a851a
ethanpil <ethan@piliavin.com>
2020-07-13 00:01:21 -0700

    Move php files as replit deletes ~ on rebuild

--- wordpress-on-replit.sh
+++ wordpress-on-replit.sh
@@ -8,11 +8,11 @@
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/php7.2/php7.2-mysql_7.2.24-0ubuntu0.18.04.6_amd64.deb
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/php7.2/php7.2-sqlite3_7.2.24-0ubuntu0.18.04.6_amd64.deb
 for Module in $( ls php*.deb ); do dpkg -x $Module .; done
-mkdir ~/php
-cp usr/lib/php/*/* ~/php
-echo 'extension=pdo.so' > ~/php/php.ini
-mv ~/php/mysqlnd.so ~/php/A-mysqlnd.so #Ensure mysqlnd.so is loaded before mysqli.so
-for Module in $( ls ~/php/*.so ); do echo "extension=$Module" >> ~/php/php.ini; done
+mkdir ~/$REPL_SLUG/php
+cp usr/lib/php/*/* ~/$REPL_SLUG/php
+echo 'extension=pdo.so' > ~/$REPL_SLUG/php/php.ini
+mv ~/$REPL_SLUG/php/mysqlnd.so ~/$REPL_SLUG/php/A-mysqlnd.so #To ensure mysqlnd.so is loaded before mysqli.so
+for Module in $( ls ~/$REPL_SLUG/php/*.so ); do echo "extension=$Module" >> ~/$REPL_SLUG/php/php.ini; done
 rm -rf ./etc/ 
 rm -rf ./usr/
 rm php7.2-mysql_7.2.24-0ubuntu0.18.04.6_amd64.deb
@@ -26,7 +26,7 @@ rm latest.zip
 #Setup the wp-config file and salts
 #todo: generate a different salt for each key
 mv ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
-NEW_PHRASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!#$%&()*+,-.:;<=>?@^_`{|}~' | fold -w 60 | head -n 1)
+NEW_PHRASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!#$%&()*+,-.:;<=>?@^_`{|}~/$REPL_SLUG' | fold -w 60 | head -n 1)
 sed -i "s/put your unique phrase here/${NEW_PHRASE}/g" ./wordpress/wp-config.php
 
 #Download, extract and cleanup sqlite plugin for WP
@@ -40,22 +40,22 @@ cp ./sqlite-integration/db.php ..
 cd ./../../..
 wget http://archive.ubuntu.com/ubuntu/pool/main/l/less/less_551-2_amd64.deb
 for Module in $( ls less*.deb ); do dpkg -x $Module .; done
-mv ./usr/ ~
+mv ./usr/ ~/$REPL_SLUG
 rm -rf less*
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-mv wp-cli.phar ~/php/wp-cli.phar
+mv wp-cli.phar ~/$REPL_SLUG/php/wp-cli.phar
 
 #hacky way to get wp-cli working in limited repli.it shell
 cat >wordpress/wp <<EOL
 #!/bin/bash
-export PATH=$PATH:~/usr/bin:~:~/php
-php -c ~/php/php.ini ~/php/wp-cli.phar "$@"
+export PATH=$PATH:~/$REPL_SLUG/usr/bin:~/$REPL_SLUG:~/$REPL_SLUG/php
+php -c ~/$REPL_SLUG/php/php.ini ~/$REPL_SLUG/php/wp-cli.phar "$@"
 EOL
-chmod +x wp
+chmod +x ~/$REPL_SLUG/wordpress/wp
 
 #remove default repl.it code file
 rm index.php
 
 #Setup the repl to start PHP with the correct php.ini that includes our modules
-echo 'run = "php -c ~/php/php.ini -S 0.0.0.0:8000 -t wordpress/"' >> .replit
+echo 'run = "php -c ~/$REPL_SLUG/php/php.ini -S 0.0.0.0:8000 -t wordpress/"' >> .replit
