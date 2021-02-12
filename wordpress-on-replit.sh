6162ea02ca0408d1c6801ed3e9bbdd5f3562fa64
ethanpil <ethan@piliavin.com>
2021-02-12 10:20:25 -0800

    Fix filenames and paths

--- wordpress-on-replit.sh
+++ wordpress-on-replit.sh
@@ -35,9 +35,9 @@ echo '$_SERVER[ "HTTPS" ] = "on";' >> ./wordpress/wp-config.php
 #Download, extract and cleanup sqlite plugin for WP
 cd ./wordpress/wp-content/plugins
 wget https://github.com/ethanpil/wp-sqlite-integration/releases/download/1.8x/sqlite-integration.zip
-unzip wp-sqlite-integration.zip
-rm wp-sqlite-integration.zip
-cp ./wp-sqlite-integration/db.php ..
+unzip sqlite-integration.zip
+rm sqlite-integration.zip
+cp ./sqlite-integration/db.php ..
 
 #Download, extract and setup wp-cli and dependencies
 cd ./../../..
