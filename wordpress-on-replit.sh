514bea4301a56985e12be1f720fdbb40d8d14ad4
ethanpil <ethan@piliavin.com>
2021-02-15 20:37:16 -0800

    Update wordpress-on-replit.sh

--- wordpress-on-replit.sh
+++ wordpress-on-replit.sh
@@ -6,7 +6,6 @@
 
 #Download, extract and cleanup required missing PHP modules
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/php7.2/php7.2-mysql_7.2.24-0ubuntu0.18.04.7_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/main/p/php7.2/php7.2-sqlite3_7.2.24-0ubuntu0.18.04.7_amd64.deb
 for Module in $( ls php*.deb ); do dpkg -x $Module .; done
 mkdir ~/$REPL_SLUG/php
 cp usr/lib/php/*/* ~/$REPL_SLUG/php
@@ -16,7 +15,13 @@ for Module in $( ls ~/$REPL_SLUG/php/*.so ); do echo "extension=$Module" >> ~/$R
 rm -rf ./etc/ 
 rm -rf ./usr/
 rm php7.2-mysql_7.2.24-0ubuntu0.18.04.7_amd64.deb
-rm php7.2-sqlite3_7.2.24-0ubuntu0.18.04.7_amd64.deb
+
+#Download and setup portable mariadb
+cd ~/$REPL_SLUG/
+wget -O portable-mariadb.tbz2 https://raw.githubusercontent.com/pts/portable-mariadb/master/release/portable-mariadb-5.5.46.tbz2
+tar xjvf portable-mariadb.tbz2
+rm portable-mariadb.tbz2
+chmod 700 ./portable-mariadb
 
 #Download, extract and cleanup WordPress Latest
 wget https://wordpress.org/latest.zip
@@ -29,19 +34,13 @@ mv ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
 NEW_PHRASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!#$%&()*+,-.:;<=>?@^_{|}~' | fold -w 60 | head -n 1)
 sed -i "s/put your unique phrase here/${NEW_PHRASE}/g" ./wordpress/wp-config.php
 
+#set database details
+sed -e "s/database_name_here/$dbname/" wp-config.php
+sed -e "s/username_here/$dbuser/g" wp-config.php
+
 #Add SSL fix to wp-config.php for repl.it proxy
 echo '$_SERVER[ "HTTPS" ] = "on";' >> ./wordpress/wp-config.php
 
-#OLD SQLITE Plugin: Download, extract and cleanup sqlite plugin for WP
-cd ~/$REPL_SLUG/wordpress/wp-content/plugins
-wget https://github.com/ethanpil/wp-sqlite-integration/releases/download/1.8x/sqlite-integration.zip
-unzip sqlite-integration.zip
-rm sqlite-integration.zip
-cp ./sqlite-integration/db.php ..
-
-#New SQLITE Plugin: Download, extract and cleanup sqlite plugin for WP
-#cd ~/$REPL_SLUG/wordpress/wp-content
-#wget https://raw.githubusercontent.com/aaemnnosttv/wp-sqlite-db/master/src/db.php
 
 #Download, extract and setup wp-cli and dependencies
 cd ~
